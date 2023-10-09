dfadab6f8073773562c1f4a4e3b26b0436004fff
loic guervin <odboy_54600@hotmail.fr>
2023-10-09 10:21:45 +0200

    ajout du projet

---
+++ FrontEnd/assets/js/script.js
@@ -0,0 +1,8 @@
+
+const btnId0 = document.querySelector(".filter_btn-id-0");
+const btnId1 = document.querySelector(".filter_btn-id-1");
+const btnId2 = document.querySelector(".filter_btn-id-2");
+const btnId3 = document.querySelector(".filter_btn-id-3");
+
+const sectiongallery = document.querySelector(".gallery"); 
+
