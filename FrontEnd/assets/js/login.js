d33dccbb57862891e0cd2763f6b28783acb31120
rgenin <romaingenin@icloud.com>
2023-10-16 15:34:58 +0200

    Mentorat - Explications filtres et login (avec code pour étude pas pour utilisation)

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,52 +1,66 @@
 //Variable//
-const  Email = document.querySelector (".email")
-const  Pwd = document.querySelector (".pwd")
+const email = document.querySelector("#email")
+const pwd = document.querySelector("#pwd")
 
 //Variable Error//
-const EmailError = document.querySelector (".email_error")
-const PwdError = document.querySelector (".pwd_error")
+const EmailError = document.querySelector(".email_error")
+const PwdError = document.querySelector(".pwd_error")
 
 //Variable connexion//
-const submit = document.querySelector (".submit")
-
+const submit = document.querySelector("#submit")
+const form = document.querySelector('form')
 
 // Fonction de connexion
 function login(id) {
 
-    console.log(id);
     EmailError.innerHTML = "";
     PwdError.innerHTML = "";
 
+    let sendData = true;
+
     //Vérification des caractère et message error (Email)//
-    if (!id.email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/g)) {
+    if (id.email.length === 0) {
         const p = document.createElement("p");
         p.innerHTML = "Email non valide";
         EmailError.appendChild(p);
-        return;
-        }
+        sendData = false
+    }
 
     //Vérification des caractère et message error (pwd = Mot de passe)//
-    if (id.pwd.length < 5 && !id.pwd.match(/^[a-zA-Z0-9]+$/g)) {
+    if (id.pwd.length === 0) {
         const p = document.createElement("p");
         p.innerHTML = "Mot de passe non valide";
         PwdError.appendChild(p);
-        return;
-        }
-        // Si couple email/mdp correct
-    else if (result.token) {
-        localStorage.setItem("token", result.token);
-        window.location.href = "index.html";
+        sendData = false
     }
-    
- }
+
+
+    if(sendData) {
+        /*fetch('http://localhost:5678/api/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(id)
+        })
+            .then(response => response.json())
+            .then(result => {
+                /!* STOCKET TOKEN OU AVERTIR USER *!/
+            })*/
+    }
+}
 
 //Connexion//
 // Permet au click d'envoyer les données Email, Pwd(mot de passe) //
 submit.addEventListener("click", () => {
     let user = {
-        Email: email.value,
-        Pwd: pwd.value
+        email: email.value,
+        pwd: pwd.value
     };
 
     login(user);
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 })
--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,4 +1,3 @@
-
 const btnId0 = document.querySelector(".filter_btn-id-0");
 const btnId1 = document.querySelector(".filter_btn-id-1");
 const btnId2 = document.querySelector(".filter_btn-id-2");
@@ -8,65 +7,77 @@ const sectiongallery = document.querySelector(".gallery");
 
 //fonction de recherche de document//
 async function getProjects() {
-
-const response = await fetch('http://localhost:5678/api/works');
-return await response.json();
-
+    const response = await fetch('http://localhost:5678/api/works');
+    return await response.json();
 }
+
 ////
 function afficherGalerie(projects, idFiltre) {
-if(idFiltre !== null) {
-    projects = projects.filter(projects => projects.categoryId === idFiltre);
-}
+    if (idFiltre !== null) {
+        projects = projects.filter(projects => projects.categoryId === idFiltre);
+    }
 
 //placement dans la div gallery //
-sectiongallery.innerHTML = '';
+    sectiongallery.innerHTML = '';
 
-projects.forEach((project) => {
-    //Récupération des projets //
-    const projectContainer = document.createElement('figure');
+    projects.forEach((project) => {
+            //Récupération des projets //
+            const projectContainer = document.createElement('figure');
 
-    //Récupération de l'image //
-    const image = document.createElement('img');
-    image.src = project.imageUrl;
-    image.setAttribute('alt', project.title);
-    projectContainer.appendChild(image);
+            //Récupération de l'image //
+            const image = document.createElement('img');
+            image.src = project.imageUrl;
+            image.setAttribute('alt', project.title);
+            projectContainer.appendChild(image);
 
-    //Récupération du titre //
-    const title = document.createElement('figcaption');
-    title.innerHTML = project.title;
-    projectContainer.appendChild(title);
+            //Récupération du titre //
+            const title = document.createElement('figcaption');
+            title.innerHTML = project.title;
+            projectContainer.appendChild(title);
 
-    //Renvoie les données dans la gallery//
-    sectiongallery.appendChild(projectContainer)
+            //Renvoie les données dans la gallery//
+            sectiongallery.appendChild(projectContainer)
         }
     );
 }
 
 changeId();
+
 //Change l'affichage au moment du click par rapport a l'id//
 function changeId() {
 
     getProjects().then((projects) => {
         afficherGalerie(projects, null);
-    //bouton btnId0 = "Tous" // click fonction affiche toute la galerie //
-    btnId0.addEventListener('click', ()=> {
-    //affiche toutes les id //
-        afficherGalerie(projects, null);
-            });
-    //bouton btnId1 = "Objets" // click fonction affiche les images de la galerie sous id "1" //
-    btnId1.addEventListener('click', ()=> { 
-    // affiche toutes les id 1 //
-        afficherGalerie(projects, 1);
-            });
-    //bouton btnId2 = "Appartements"//
-    btnId2.addEventListener('click', ()=> {
-        afficherGalerie(projects, 2);
-            })
-    //bouton btnId3 = "Hotels et restaurants"//
-    btnId3.addEventListener('click', ()=> {
-        afficherGalerie(projects, 3);
+        //bouton btnId0 = "Tous" // click fonction affiche toute la galerie //
+        btnId0.addEventListener('click', () => {
+            //affiche toutes les id //
+            afficherGalerie(projects, null);
+            manageFilter(btnId0);
+        });
+        //bouton btnId1 = "Objets" // click fonction affiche les images de la galerie sous id "1" //
+        btnId1.addEventListener('click', () => {
+            // affiche toutes les id 1 //
+            afficherGalerie(projects, 1);
+            manageFilter(btnId1);
+        });
+        //bouton btnId2 = "Appartements"//
+        btnId2.addEventListener('click', () => {
+            afficherGalerie(projects, 2);
+            manageFilter(btnId2);
+        })
+        //bouton btnId3 = "Hotels et restaurants"//
+        btnId3.addEventListener('click', () => {
+            afficherGalerie(projects, 3);
+            manageFilter(btnId3);
         })
     })
-    
+}
+
+function manageFilter(filtre) {
+    // Supprime la classe du filtre actuel
+    const filtreActuel = document.querySelector('.filter_btn-active');
+    filtreActuel.classList.remove('filter_btn-active')
+
+    // Active le filtre cliqué
+    filtre.classList.add('filter_btn-active');
 }
