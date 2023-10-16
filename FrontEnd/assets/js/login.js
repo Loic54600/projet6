3cd28aca2bb63a76421ec4828b911e05f3f36538
loic guervin <odboy_54600@hotmail.fr>
2023-10-16 10:26:18 +0200

    login

---
+++ FrontEnd/assets/js/login.js
@@ -0,0 +1,45 @@
+//Variable//
+const  Email = document.querySelector (".email")
+const  Pwd = document.querySelector (".pwd")
+
+//Variable Error//
+const EmailError = document.querySelector (".email_error")
+const PwdError = document.querySelector (".pwd_error")
+
+//Variable connexion//
+const submit = document.querySelector (".submit")
+
+
+// Fonction de connexion
+function login(id) {
+
+    console.log(id);
+    EmailError.innerHTML = "";
+    PwdError.innerHTML = "";
+
+    //Vérification des caractère et message error (Email)//
+    if (!id.email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/g)) {
+        const p = document.createElement("p");
+        p.innerHTML = "Email non valide";
+        EmailError.appendChild(p);
+        return;
+    }
+
+    //Vérification des caractère et message error (pwd = Mot de passe)//
+    if (id.pwd.length < 5 && !id.pwd.match(/^[a-zA-Z0-9]+$/g)) {
+        const p = document.createElement("p");
+        p.innerHTML = "Mot de passe non valide";
+        PwdError.appendChild(p);
+        return;
+    }
+    }
+//Connexion//
+// Permet au click d'envoyer les données Email, Pwd(mot de passe) //
+submit.addEventListener("click", () => {
+    let user = {
+        Email: email.value,
+        Pwd: pwd.value
+    };
+
+    login(user);
+})
--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -4,5 +4,69 @@ const btnId1 = document.querySelector(".filter_btn-id-1");
 const btnId2 = document.querySelector(".filter_btn-id-2");
 const btnId3 = document.querySelector(".filter_btn-id-3");
 
-const sectiongallery = document.querySelector(".gallery"); 
+const sectiongallery = document.querySelector(".gallery");
 
+//fonction de recherche de document//
+async function getProjects() {
+
+const response = await fetch('http://localhost:5678/api/works');
+return await response.json();
+
+}
+////
+function afficherGalerie(projects, idFiltre) {
+if(idFiltre !== null) {
+    projects = projects.filter(projects => projects.categoryId === idFiltre);
+}
+
+//placement dans la div gallery //
+sectiongallery.innerHTML = '';
+
+projects.forEach((project) => {
+    //Récupération des projets //
+    const projectContainer = document.createElement('figure');
+
+    //Récupération de l'image //
+    const image = document.createElement('img');
+    image.src = project.imageUrl;
+    image.setAttribute('alt', project.title);
+    projectContainer.appendChild(image);
+
+    //Récupération du titre //
+    const title = document.createElement('figcaption');
+    title.innerHTML = project.title;
+    projectContainer.appendChild(title);
+
+    //Renvoie les données dans la gallery//
+    sectiongallery.appendChild(projectContainer)
+        }
+    );
+}
+
+changeId();
+//Change l'affichage au moment du click par rapport a l'id//
+function changeId() {
+
+    getProjects().then((projects) => {
+        afficherGalerie(projects, null);
+    //bouton btnId0 = "Tous" // click fonction affiche toute la galerie //
+    btnId0.addEventListener('click', ()=> {
+    //affiche toutes les id //
+        afficherGalerie(projects, null);
+            });
+    //bouton btnId1 = "Objets" // click fonction affiche les images de la galerie sous id "1" //
+    btnId1.addEventListener('click', ()=> { 
+    // affiche toutes les id 1 //
+        afficherGalerie(projects, 1);
+            });
+    //bouton btnId2 = "Appartements"//
+    btnId2.addEventListener('click', ()=> {
+        afficherGalerie(projects, 2);
+            })
+    //bouton btnId3 = "Hotels et restaurants"//
+    btnId3.addEventListener('click', ()=> {
+        afficherGalerie(projects, 3);
+        })
+    })
+    
+}
