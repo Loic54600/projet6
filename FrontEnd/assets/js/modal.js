d2c6734bd443afb084bf9bce70d841051c543e2a
loic guervin <odboy_54600@hotmail.fr>
2023-10-23 18:05:32 +0200

    modification modale

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -3,11 +3,7 @@ document.getElementById('btn-modal').addEventListener('click', function() {
     document.getElementById('overlay').classList.add('visible');
     document.getElementById('modal').classList.add('visible');
   });
-//Permet au bouton modal de fermer la modal//
-document.getElementById('close-btn').addEventListener('click', function() {
-    document.getElementById('overlay').classList.remove('visible');
-    document.getElementById('modal').classList.remove('visible');
-});
+
 
 
 
