8e462ab9bdddd1be155249313840827c81bf7ee6
loic guervin <odboy_54600@hotmail.fr>
2023-10-26 11:40:05 +0200

    modification login

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,7 +1,7 @@
 //Variable //
 const email = document.querySelector("#email")
 const pwd = document.querySelector("#pwd")
-const error = document.querySelector(".error"); 
+const error = document.querySelector(".error");
 
 //Variable Error //
 const EmailError = document.querySelector(".email_error")
@@ -24,15 +24,15 @@ function login(id) {
         EmailError.appendChild(p);
         sendData = false
     }
-    //Vérification message error (pwd = Mot de passe)//
-    if (id.pwd.length === 0) {
+    //Vérification message error (password = Mot de passe)//
+    if (id.password.length === 0) {
         const p = document.createElement("p");
         //Message erreur //
         p.innerHTML = "Mot de passe non valide";
         PwdError.appendChild(p);
         sendData = false
     }
-    if(sendData) {
+    if (sendData) {
         fetch('http://localhost:5678/api/users/login', {
             method: 'POST',
             headers: {
@@ -43,43 +43,45 @@ function login(id) {
             .then(response => response.json())
             .then(result => {
                 console.log(result);
-        //si le resultat ne correspond pas, un message d'erreur s'affiche //
-        if (result.error || result.message) {
-            const p = document.createElement("p");
-            p.innerHTML = "La combinaison e-mail/mot de passe est incorrecte";
-            PwdError.appendChild(p);
-        //Si le token est correct, il a connexion //
-        } else if (result.token) {
-            localStorage.setItem("token", result.token);
-            // retour a la page index si connexion //
-            window.location.href = "index.html";
-        }
-    })
-    //error de log //
-    .catch(error => 
-        console.log(error));
+                //si le resultat ne correspond pas, un message d'erreur s'affiche //
+                if (result.error || result.message) {
+                    const p = document.createElement("p");
+                    p.innerHTML = "La combinaison e-mail/mot de passe est incorrecte";
+                    PwdError.appendChild(p);
+                    //Si le token est correct, il a connexion //
+                } else if (result.token) {
+                    localStorage.setItem("token", result.token);
+                    // retour a la page index si connexion //
+                    window.location.href = "index.html";
+                }
+            })
+            //error de log //
+            .catch(error =>
+                console.log(error));
     }
 }
 
 //Connexion//
 // Permet au click d'envoyer les données Email, Pwd(mot de passe) //
 submit.addEventListener("click", () => {
-    let user = {
-        email: email.value,
-        pwd: pwd.value
-    };
-    login(user);
+        let user = {
+            email: email.value,
+            password: pwd.value
+        };
+        login(user);
     }
 )
 //Envoie les données du formulaire //
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
+    console.log('in')
+        e.preventDefault();
     }
 )
 
-const Supprtokken = document.querySelector(".error"); 
+const Supprtokken = document.querySelector(".error");
+
 //Si la personne est déjà connecter, on supprime le token //
-function Supprtokken() {
+function deleteToken() {
     if (localStorage.getItem("token")) {
         localStorage.removeItem("token");
 
@@ -87,6 +89,7 @@ function Supprtokken() {
         //Message de reconnexion //
         p.innerHTML = "Veuillez vous reconnecter";
         Supprtokken.appendChild(p);
-        return;
     }
 }
+
+deleteToken();
--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -1,8 +1,55 @@
+//Variable récupération du token + class//
+const token = localStorage.getItem("token");
+const deconnexion = document.querySelector(".co-deco");
+
+//Fonction changement de connexion en déconnexion//
+adminDeco()
+
+function adminDeco() {
+    document.querySelectorAll(".deconnexion").forEach(a => {
+        if (token === null) {
+            return;
+        }
+        else {
+            a.removeAttribute("style")
+            deconnexion.innerHTML = "Logout";
+        }
+    });
+}
+//Fonction affiche le bouton "modifier"//
+modifier()
+
+function modifier() {
+  document.querySelectorAll(".btn-modal").forEach(a => {
+      if (token === null) {
+          return;
+      }
+      else {
+          a.removeAttribute("style");
+      }
+  });
+}
 //Permet au click du bouton d'appeller la modal //
 document.getElementById('btn-modal').addEventListener('click', function() {
     document.getElementById('overlay').classList.add('visible');
     document.getElementById('modal').classList.add('visible');
   });
+
+//Ajout de la galerie dans la modal//
+
+
+
+
+//Permet au click du bouton d'appeller la modal "ajout photo"//
+
+
+
+
+//Genere le bouton "modifier une fois connecter"//
+
+
+
+
 
 
 
